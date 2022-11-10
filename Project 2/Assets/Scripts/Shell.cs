0e5cf6b3aa8059c208642922c2faeff8b3e28481
Diogo Guerra <100213353+Deni6662@users.noreply.github.com>
2022-11-10 19:17:32 +0000

    Second Work

---
+++ Project 2/Assets/Scripts/Shell.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shell : MonoBehaviour
+{
+    public GameObject explosion;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "tank")
+        {
+            GameObject exp = Instantiate(explosion, this.transform.position, Quaternion.identity);
+            Destroy(exp, 0.5f);
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+
+    }
+}
---
+++ Project 2/Assets/ShellM.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellM : MonoBehaviour
+{
+    float speed = 1;
+
+    void Update()
+    {
+        this.transform.Translate(0, (speed * Time.deltaTime) / 2.0f, speed * Time.deltaTime);
+    }
+}
