c2d1b3311cd7649d89c7a10fa2768dc7f843af8f
Diogo Guerra <100213353+Deni6662@users.noreply.github.com>
2022-12-04 15:04:27 +0000

    Project 4

---
+++ cowd simulation/Project 4/Assets/AIControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class AIControl:MonoBehaviour {
+
+    GameObject[] goalLocations;
+    UnityEngine.AI.NavMeshAgent agent;
+    Animator anim;
+
+    void Start() {
+        goalLocations = GameObject.FindGameObjectsWithTag("goal");
+        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
+        anim = this.GetComponent<Animator>();
+        anim.SetTrigger("isWalking");
+    }
+
+    void Update() {
+
+        if (agent.remainingDistance<1) {
+            agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
+        }
+    }
+}
