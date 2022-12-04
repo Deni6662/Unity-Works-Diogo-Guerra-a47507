aa9810ae55f14beb990642f5c5f3c7b43fb2ec34
Diogo Guerra <100213353+Deni6662@users.noreply.github.com>
2022-12-04 14:15:56 +0000

    Project 3 - Diogo Guerra a47507

---
+++ cowd simulation/Project 3/Assets/AIcontroller.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIcontroller : MonoBehaviour {
+
+    public GameObject goal;
+    NavMeshAgent agent;
+
+
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.SetDestination(goal.transform.position);
+    }
+
+}
